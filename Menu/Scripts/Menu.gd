b994cc72ee0214dddc3ffef1781c10cbd8429d74
DesertSkate <robertschaz@gmail.com>
2021-05-19 15:11:38 -0600

    Init

---
+++ Menu/Scripts/Menu.gd
@@ -0,0 +1,33 @@
+extends VBoxContainer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_parent().get_node("FadeIn").get_node("AnimationPlayer").play("FadeIn")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_PlayButton_pressed():
+	get_tree().change_scene("res://Menu/Typewriter Cutscene.tscn")
+
+
+func _on_CreditButton_pressed():
+	get_tree().change_scene("res://Menu/Credits.tscn")
+
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	pass
---
+++ Menu/Scripts/Typewriter Cutscene.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("RightClick"):
+		get_tree().change_scene("res://Menu/Menu.tscn")
